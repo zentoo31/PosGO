ec4768fa88042836328a34f7fa56ee7e66424914
Diego Pineda <zentoo31@gmail.com>
2025-06-26 17:49:37 -0500

    add addProduct component with basic layout

---
+++ app/addProduct.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const addProduct = () => {
+  return (
+    <View>
+      <Text>addProduct</Text>
+    </View>
+  )
+}
+
+export default addProduct
