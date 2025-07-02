ed235fe13ca1a7f035a50c1134d1cf1dd07b023f
Diego Pineda <zentoo31@gmail.com>
2025-07-02 14:05:52 -0500

    refactor Index component to manage welcome page visibility using AsyncStorage

--- app/index.tsx
+++ app/index.tsx
@@ -1,27 +1,50 @@
-import { Link } from "expo-router";
-import { useVideoPlayer, VideoView } from 'expo-video';
-import { View } from "react-native";
-
-
-const videoSource =
-  '../assets/video/pos_video_hd_1920_1080_30fps.mp4';
+import WelcomePage from "@/components/welcomePage";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { Redirect } from "expo-router";
+import { useEffect, useState } from "react";
+import { ActivityIndicator, View } from "react-native";
 
 export default function Index() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [hasSeenWelcome, setHasSeenWelcome] = useState(false);
 
-  const player = useVideoPlayer(videoSource, (player) => {
-    player.loop = true;
-    player.muted = true;
-    player.play();
-  });
+  useEffect(() => {
+    const checkWelcomePage = async () => {
+      try {
+        const seen = await AsyncStorage.getItem("hasSeenWelcome");
+        if (seen) {
+          setHasSeenWelcome(true);
+        }
+      } catch (error) {
+        console.error("Error checking welcome page:", error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
 
-  return (
-    <View className="flex-1 justify-center items-center ">
-      <VideoView
-        player={player}
-        allowsFullscreen={false}
-        allowsPictureInPicture={false}
-      />
-      <Link href={"/login"}>Iniciar sesión</Link>
-    </View>
-  );
+    checkWelcomePage();
+  }, []);
+
+  const handleWelcomeComplete = async (): Promise<void> => {
+    try {
+      await AsyncStorage.setItem('hasSeenWelcome', JSON.stringify(true));
+      setHasSeenWelcome(true);
+    } catch (error) {
+      console.error('Error saving to AsyncStorage:', error);
+    }
+  };
+
+  if (isLoading) {
+    return (
+      <View className="flex-1 justify-center items-center">
+        <ActivityIndicator size="large" color="#0000ff" />
+      </View>
+    );
+  }
+
+  return hasSeenWelcome ? (
+    <Redirect href={'/login'}/>
+  ): (
+    <WelcomePage onComplete={handleWelcomeComplete} />
+  )
 }
