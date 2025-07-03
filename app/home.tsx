5ab9cb7703da4e2a0e3aebf931b8967ff47336ac
Diego Pineda <zentoo31@gmail.com>
2025-07-02 19:37:28 -0500

    refactor Home component to improve navigation and UI structure by replacing Link with TouchableOpacity

--- app/home.tsx
+++ app/home.tsx
@@ -2,11 +2,12 @@ import RegisterProd from '@/components/registerProd';
 import Searcher from '@/components/searcher';
 import Feather from '@expo/vector-icons/Feather';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { Link } from 'expo-router';
+import { useRouter } from 'expo-router';
 import React, { useEffect, useState } from 'react';
-import { KeyboardAvoidingView, Platform, Pressable, SafeAreaView, StatusBar, Text, View } from 'react-native';
+import { KeyboardAvoidingView, Platform, SafeAreaView, StatusBar, Text, TouchableOpacity, View } from 'react-native';
 
 const Home = () => {
+  const router = useRouter();
   const [addition, setAddition] = useState<number>(0);
 
   const getAdditionValue = async () => {
@@ -32,19 +33,17 @@ const Home = () => {
             paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
           }}
         >
-          <View className='flex-row items-center justify-between px-4 py-3'>
+          <View className='flex-row items-center justify-between px-4 py-4'>
             <View className='flex-row items-center'>
               <Feather name="shopping-bag" size={24} color="white" />
               <Text className='text-2xl font-bold text-white ml-2'>PosGo</Text>
             </View>
-            <Link href="/cart" asChild>
-              <Pressable className='flex-row items-center gap-2'>
+            <TouchableOpacity onPress={() => router.push('/cart')} className='flex-row items-center gap-2 bg-blue-500 px-4 rounded-lg py-1'>
                 <Feather name="shopping-cart" size={24} color="white" />
                 <Text className='text-white text-lg mr-2'>
                   S/. {addition.toFixed(2)}
                 </Text>
-              </Pressable>
-            </Link>
+            </TouchableOpacity>
           </View>
         </View>
 
