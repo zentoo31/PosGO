ffdf89fa89b0db11b484a39f01ea37354d812f59
Diego Pineda <zentoo31@gmail.com>
2025-07-04 08:12:09 -0500

    add disable atribute for qr textinput

--- components/registerProd.tsx
+++ components/registerProd.tsx
@@ -10,7 +10,7 @@ const RegisterProd = () => {
     <View className='p-4 bg-white rounded-t-lg border-b-[0.5px] border-gray-300 flex-row gap-2'>
       <View className='h-12 rounded-md flex-row items-center px-5 border-gray-300 border-[1px]'>
         <Feather name="codesandbox" size={24} color="gray" />
-        <TextInput className='h-full px-5' placeholder='Escanea el código qr' placeholderTextColor={'gray'}/>
+        <TextInput className='h-full px-5' placeholder='Escanea el código qr' placeholderTextColor={'gray'} editable={false}/>
       </View>
       <TouchableOpacity className='bg-blue-500 rounded-lg flex-row items-center p-2' onPress={() => router.push('/addProduct')} >
         <Feather name="plus" size={24} color="white" />
