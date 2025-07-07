023c02ea66d596ddcbb3519bb5a5f345aecb31c5
Diego Pineda <zentoo31@gmail.com>
2025-07-06 23:47:55 -0500

    fix: refactor product fetching logic and implement search functionality

--- components/searcher.tsx
+++ components/searcher.tsx
@@ -16,14 +16,6 @@ import {
 export default function Searcher({ onAddItem }: { onAddItem: () => void }) {
   const productService = new ProductService();
   const [busqueda, setBusqueda] = useState('');
-  const [datos] = useState([
-    { id: '1', nombre: 'Manzana', pic: 'https://www.recetasnestle.com.pe/sites/default/files/2022-07/tipos-de-manzana-royal-gala.jpg', price: 1.2, category: 'Frutas', stock: 10 },
-    { id: '2', nombre: 'Banana', pic: 'https://media.gettyimages.com/id/173242750/es/foto/racimo-de-pl%C3%A1tanos.jpg?s=612x612&w=gi&k=20&c=H6yBfWKtDnM7sRqcETrnis9V9XnkAiPaOnP77sOKooo=', price: 0.5, category: 'Frutas', stock: 10 },
-    { id: '3', nombre: 'Leche', pic: 'https://incalec.org/storage/2021/07/milk-and-glass-scaled.jpg', price: 1.0, category: 'Lácteos', stock: 10 },
-    { id: '4', nombre: 'Pan', pic: 'https://panoli.pe/wp-content/uploads/2020/02/IMG_0315-1.jpg', price: 1.5, category: 'Panadería', stock: 10 },
-    { id: '5', nombre: 'Carne Molida', pic: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnwhclycPN_bAZSaZ8lbHDF3eFEb7Jlvo8RA&s', price: 3.0, category: 'Carnes', stock: 10 },
-    { id: '6', nombre: 'Arroz', pic: 'https://www.allrecipes.com/thmb/RKpnSHLUDT2klppYgx8jAF47GyM=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/52490-PuertoRicanSteamedRice-DDMFS-061-4x3-3c3da714aa614037ad1c135ec303526d.jpg', price: 0.8, category: 'Granos', stock: 10 }
-  ]);
   const [products, setProducts] = useState<Product[]>([]);
   const [loading, setLoading] = useState(true);
 
@@ -37,19 +29,36 @@ export default function Searcher({ onAddItem }: { onAddItem: () => void }) {
     } finally {
       setLoading(false);
     }
-  }
+  };
+
+  const buscarProductos = async (query: string) => {
+    try {
+      setLoading(true);
+      if (query.trim() === '') {
+        const allProducts = await productService.getAllProducts();
+        setProducts(allProducts);
+      } else {
+        const searched = await productService.getProductsByName(query);
+        setProducts(searched);
+      }
+    } catch (error) {
+      console.error('Error buscando productos:', error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      fetchProducts();
-    }, 100);
+      buscarProductos(busqueda);
+    }, 500); // retraso de 500ms entre cada búsqueda
+
     return () => clearTimeout(timer);
-  }, []);
-
-
-  const datosFiltrados = datos.filter(item =>
-    item.nombre.toLowerCase().includes(busqueda.toLowerCase())
-  );
+  }, [busqueda]);
 
   const addAddition = async (price: number, nombre: string, pic: string) => {
     try {
@@ -65,7 +74,6 @@ export default function Searcher({ onAddItem }: { onAddItem: () => void }) {
       console.error('Error al actualizar "addition":', error);
     }
   };
-
 
   const renderItem = ({ item }: { item: { id: string; nombre: string, pic: string; price: number, category: string, stock: number } }) => (
     <View className='flex-1 m-1 flex-col border-[0.5px] border-gray-300 rounded-lg'>
@@ -120,7 +128,6 @@ export default function Searcher({ onAddItem }: { onAddItem: () => void }) {
           contentContainerStyle={{ paddingVertical: 8 }}
           renderItem={renderItem}
           ListEmptyComponent={
-
             <Text className="text-center text-gray-400 mt-6">
               No hay resultados
             </Text>
