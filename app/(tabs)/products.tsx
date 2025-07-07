476a55b3f8e1ae9925c71c0db1068ed9438073ba
Diego Pineda <zentoo31@gmail.com>
2025-07-07 11:20:38 -0500

    feat: refactor Products component to use SafeAreaView and KeyboardAvoidingView for improved layout

--- app/(tabs)/products.tsx
+++ app/(tabs)/products.tsx
@@ -1,11 +1,35 @@
+import SearcherEdit from '@/components/searcherEdit'
 import React from 'react'
-import { Text, View } from 'react-native'
+import {
+  KeyboardAvoidingView,
+  Platform,
+  SafeAreaView,
+  StatusBar,
+  Text,
+  View
+} from 'react-native'
 
 const Products = () => {
   return (
-    <View>
-      <Text>products</Text>
-    </View>
+    <SafeAreaView className='flex-1 bg-[#16429E]'>
+      <StatusBar backgroundColor="#16429E" barStyle="light-content" translucent />
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        className='flex-1'
+      >
+        <View style={{ paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0 }}>
+          <View className='flex-row items-center justify-center px-4 py-4'>
+            <Text className='text-2xl font-bold text-white'>Productos</Text>
+          </View>
+        </View>
+        <View className='bg-[#F9FAFB] px-4 pt-6 flex-1 '>
+          <View className='shadow-lg border-[0.5px] border-gray-300 rounded-md bg-white flex-1'>
+            <SearcherEdit />
+          </View>
+        </View>
+
+      </KeyboardAvoidingView>
+    </SafeAreaView>
   )
 }
 
