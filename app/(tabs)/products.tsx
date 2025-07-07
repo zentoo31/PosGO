aa8472d09c461805e2935fe61084d6a7cb274f70
Diego Pineda <zentoo31@gmail.com>
2025-07-07 01:10:04 -0500

    feat: add Products component to display product information

---
+++ app/(tabs)/products.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const Products = () => {
+  return (
+    <View>
+      <Text>products</Text>
+    </View>
+  )
+}
+
+export default Products
